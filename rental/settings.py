96ada6daa0063d4da311c3d39e93a1dfacae80b5
Shafiul-Sabbir <sabbirvai82@gmail.com>
2025-04-14 03:06:41 +0600

    updated staticfiles successfully

--- rental/settings.py
+++ rental/settings.py
@@ -139,6 +139,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
